a2930fd8e007adaa3cdb44ca12a7df4ba15f586d
Aminu Muhammad <aminuastroboy@gmail.com>
2025-09-13 22:30:35 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E6399B",
+        secondary: "#9333EA",
+        dark: "#0f0f0f"
+      }
+    }
+  },
+  plugins: []
+}
