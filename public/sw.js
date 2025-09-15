55e3682e727190d82de29cacb868c33a8aab678b
Aminu Muhammad <aminuastroboy@gmail.com>
2025-09-15 18:21:54 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,25 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(self.skipWaiting());
+// Simple service worker for offline caching
+const CACHE_NAME = "cycle-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
