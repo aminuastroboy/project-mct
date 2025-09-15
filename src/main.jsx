1821c96c304185efc915c976db2f4c0d26079af0
Aminu Muhammad <aminuastroboy@gmail.com>
2025-09-15 18:20:03 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
 
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
-      .then(() => console.log("✅ Service Worker registered"))
-      .catch(err => console.log("❌ SW registration failed:", err));
-  });
+  })
 }
